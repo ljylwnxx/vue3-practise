e45b313d212e7a8b982f7b35b23819bb8043466b
ljylwnxx <ljylwnxx@163.com>
2023-08-04 17:23:51 +0800

    fix:isref

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,33 @@
 <template>
   <div>
     <h1>人物简介</h1>
-    <p>姓名：{{data.name}}</p>
-    <p>年龄：{{data.age}}岁</p>
-    <p>爱好：{{data.hobbies.join('、')}}</p>
-    <p>地址：{{data.addres.provice}} - {{data.addres.city}}</p>
-    <p>描述：{{data.description}}</p>
-    <button @click="modifyInfo">
-    修改信息
-    </button> 
+    <p>姓名：{{name}}</p>
+    <p>年龄：{{age}}岁</p>
+    <p>爱好：{{hobbies.join('、')}}</p>
+    <p>地址：{{address.provice}} - {{ toRefCity }} </p>
+    <p>描述：{{description}}</p>
   </div> 
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-    const data = reactive ({
-      name: 'pupu',
-      age: 10,
-      hobbies: ['唱歌', '画画'],
-      addres: {
-        provice: '浙江省',
-        city: '杭州市'
-      },
-      description: '一点也不可爱，不喜欢吃蜂蜜！'
-      })
+import { ref, reactive, toRef, toRefs, isRef } from 'vue'
+    const name = ref('pupu')
+    const age = 10
+    const hobbies = ref(['唱歌', '画画'])
+    const address = reactive({
+      provice: '浙江省',
+      city: '杭州市'
+    })
+    const description = ref('一点也不可爱，不喜欢吃蜂蜜！')
+    const toRefCity = toRef(address, 'city')
+    const toRefsAddress = toRefs(address)
 
-    const modifyInfo = () => {
-        data.name = 'wnxx'
-        data.age = 3 
-        data.hobbies = ['打羽毛球', '旅游']
-        data.addres.provice = '云南省'
-        data.addres.city = '丽江市'
-        data.description = '非常的可爱，特别喜欢吃蜂蜜！'
-        console.log(data.name,data.hobbies,data.addres)
-    } 
+    // 判断是否为ref对象
+    console.log(isRef(name), 'isRefname')
+    console.log(isRef(age), 'isRefage')
+    console.log(isRef(hobbies),'isRefhobbies')
+    console.log(isRef(address),'isRefaddress')
+    console.log(isRef(description),'isRefdescription')
+    console.log(isRef(toRefCity),'isReftoRefCity')
+    console.log(isRef(toRefsAddress),'isReftoRefsAddress')
 </script>
--- src/components/isRef.vue
+++ src/components/isRef.vue
@@ -3,7 +3,7 @@
     <h1>人物简介</h1>
     <p>姓名：{{name}}</p>
     <p>年龄：{{age}}岁</p>
-    <p>爱好：{{hobby.join('、')}}</p>
+    <p>爱好：{{hobbies.join('、')}}</p>
     <p>地址：{{address.provice}} - {{ toRefCity }} </p>
     <p>描述：{{description}}</p>
   </div> 
@@ -13,7 +13,7 @@
 import { ref, reactive, toRef, toRefs, isRef } from 'vue'
     const name = ref('pupu')
     const age = 10
-    const hobby = ref(['唱歌', '画画'])
+    const hobbies = ref(['唱歌', '画画'])
     const address = reactive({
       provice: '浙江省',
       city: '杭州市'
@@ -25,7 +25,7 @@ import { ref, reactive, toRef, toRefs, isRef } from 'vue'
     // 判断是否为ref对象
     console.log(isRef(name), 'isRefname')
     console.log(isRef(age), 'isRefage')
-    console.log(isRef(hobby),'isRefhobby')
+    console.log(isRef(hobbies),'isRefhobbies')
     console.log(isRef(address),'isRefaddress')
     console.log(isRef(description),'isRefdescription')
     console.log(isRef(toRefCity),'isReftoRefCity')
