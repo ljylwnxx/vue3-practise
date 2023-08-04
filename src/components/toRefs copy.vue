6e271d467a6870738410566c775f1110b6149667
ljylwnxx <ljylwnxx@163.com>
2023-08-04 16:58:16 +0800

    fix:torefs

--- src/components/toRefs copy.vue
+++ src/components/toRefs copy.vue
@@ -3,7 +3,7 @@
     <h1>人物简介</h1>
     <p>姓名：{{toRefsdata.name.value}}</p>
     <p>年龄：{{toRefsdata.age.value}}岁</p>
-    <p>爱好：{{toRefsdata.hobby.value.join('、')}}</p>
+    <p>爱好：{{toRefsdata.hobbies.value.join('、')}}</p>
     <p>地址：{{toRefsdata.addressprovice.value}} - {{toRefsdata.addresscity.value}}</p>
     <p>描述：{{toRefsdata.description.value}}</p>
     <button @click="changeReactive">
@@ -17,7 +17,7 @@ import { reactive, toRefs } from 'vue'
         const data = reactive ({
         name: 'pupu',
         age: 10,
-        hobby: ['唱歌', '画画'],
+        hobbies: ['唱歌', '画画'],
         addressprovice: '浙江省',
         addresscity: '杭州市',
         description: '一点也不可爱，不喜欢吃蜂蜜！'
@@ -36,7 +36,7 @@ import { reactive, toRefs } from 'vue'
         const changeReactive = () => {
           toRefsdata.name.value = 'wnxx'
           toRefsdata.age.value = 3 
-          toRefsdata.hobby.value = ['打羽毛球', '旅游']
+          toRefsdata.hobbies.value = ['打羽毛球', '旅游']
           toRefsdata.addressprovice.value = '云南省'
           toRefsdata.addresscity.value = '丽江市'
           toRefsdata.description.value = '非常的可爱，特别喜欢吃蜂蜜！'
--- src/components/toRefs.vue
+++ src/components/toRefs.vue
@@ -3,7 +3,7 @@
     <h1>人物简介</h1>
     <p>姓名：{{name}}</p>
     <p>年龄：{{age}}岁</p>
-    <p>爱好：{{hobby.join('、')}}</p>
+    <p>爱好：{{hobbies.join('、')}}</p>
     <p>地址：{{addressprovice}} - {{addresscity}}</p>
     <p>描述：{{description}}</p>
     <button @click="changeReactive">
@@ -17,7 +17,7 @@ import { reactive, toRefs } from 'vue'
         const data = reactive ({
         name: 'pupu',
         age: 10,
-        hobby: ['唱歌', '画画'],
+        hobbies: ['唱歌', '画画'],
         addressprovice: '浙江省',
         addresscity: '杭州市',
         description: '一点也不可爱，不喜欢吃蜂蜜！'
@@ -30,7 +30,7 @@ import { reactive, toRefs } from 'vue'
         const {
           name,
           age,
-          hobby,
+          hobbies,
           addressprovice,
           addresscity,
           description
@@ -40,11 +40,11 @@ import { reactive, toRefs } from 'vue'
         const changeReactive = () => {
             name.value = 'wnxx'
             age.value = 3 
-            hobby.value = ['打羽毛球', '旅游']
+            hobbies.value = ['打羽毛球', '旅游']
             addressprovice.value = '云南省'
             addresscity.value = '丽江市'
             description.value = '非常的可爱，特别喜欢吃蜂蜜！'
             // 打印toRefs之后的数据
-            console.log(name, age,hobby,addressprovice,addresscity,description, 'toRefsdata')
+            console.log(name, age,hobbies,addressprovice,addresscity,description, 'toRefsdata')
         } 
 </script>
