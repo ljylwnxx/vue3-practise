00eb6d5fc3c797e88ffcb23be39d8c6c1c97ceb1
ljylwnxx <ljylwnxx@163.com>
2023-08-04 17:52:46 +0800

    fix:unref

--- src/components/unRef.vue
+++ src/components/unRef.vue
@@ -3,7 +3,7 @@
     <h1>人物简介</h1>
     <p>姓名：{{name}}</p>
     <p>年龄：{{age}}岁</p>
-    <p>爱好：{{hobby.join('、')}}</p>
+    <p>爱好：{{hobbies.join('、')}}</p>
     <p>地址：{{address.provice}} - {{ toRefCity }} </p>
     <p>描述：{{description}}</p>
   </div> 
@@ -13,7 +13,7 @@
 import { ref, reactive, toRef, toRefs, unref } from 'vue'
     const name = ref('wnxx')
     const age = 3
-    const hobby = ref(['打羽毛球', '旅游'])
+    const hobbies = ref(['打羽毛球', '旅游'])
     const address = reactive({
       provice: '云南省',
       city: '丽江市'
@@ -25,7 +25,7 @@ import { ref, reactive, toRef, toRefs, unref } from 'vue'
     // 打印
     console.log(unref(name))
     console.log(unref(age))
-    console.log(unref(hobby))
+    console.log(unref(hobbies))
     console.log(unref(address))
     console.log(unref(description))
     console.log(unref(toRefCity))
