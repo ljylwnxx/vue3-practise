616b80c213fddf7b6738897354f2e32f01ceefb2
ljylwnxx <ljylwnxx@163.com>
2023-09-05 23:10:20 +0800

    feat:provide&inject

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
-   <childComponent @trigger="handleEvent"></childComponent>
-  </div> 
+    <h1>人物简介</h1>
+    <p>姓名：{{person.name}}</p>
+    <p>年龄：{{person.age}}岁</p>
+    <p>地址：{{address.provice}} - {{address.city}}</p>
+    <button @click="modifyInfo">
+      修改数据
+    </button>
+    <ProvideInject></ProvideInject>
+  </div>
 </template>
 
 <script>
-import childComponent from './components/childComponent.vue'
-
+import { ref, reactive, provide } from 'vue'
+import ProvideInject from './components/provideInject.vue'
 export default {
   components: {
-    childComponent
+    ProvideInject
   },
-  methods: {
-    handleEvent(event) {
-      console.log(event, 'handleEvent')
+  setup() {
+    const person = ref({
+      name: 'pupu',
+      age: 10
+    })
+    const address = reactive({
+      provice: '浙江省',
+      city: '杭州市'
+    })
+    provide('changePerson',person)
+    provide('changeAddress',address)
+    const modifyInfo = () => {
+      person.value.name = 'wnxx'
+        person.value.age = 3 
+        address.provice = '云南省'
+        address.city = '丽江市'
     }
-  },
+    return {
+      person,
+      address,
+      modifyInfo
+    }
+  }
 }
 </script>
+
---
+++ src/components/provideInject.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>子组件</h1>
+    <div>接受到的数据：{{ getPerson }} - {{ getAddress }}
+    <h1>人物简介</h1>
+    <p>姓名：{{getPerson.name }}</p>
+    <p>年龄：{{getPerson.age}}岁</p>
+    <p>地址：{{getAddress.provice}}- {{ getAddress.city }}</p>
+    </div>
+  </div> 
+</template>
+
+<script setup>
+import { inject } from 'vue'
+
+    const getPerson = inject('changePerson', '这是默认值我叫pupu,今年10岁')
+    const getAddress = inject('changeAddress', '我来自浙江省-杭州市')
+    console.log(getPerson, 'getperson')
+    console.log(getAddress, 'getaddress')
+</script>
